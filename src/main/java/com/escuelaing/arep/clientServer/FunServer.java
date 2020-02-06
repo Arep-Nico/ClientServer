956d31c0f80999de7b9228b245b99ee52cb146db
NicoCardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2020-02-06 00:58:37 -0500

    httpServer read html files

--- src/main/java/com/escuelaing/arep/clientServer/FunServer.java
+++ src/main/java/com/escuelaing/arep/clientServer/FunServer.java
@@ -29,37 +29,51 @@ public class FunServer {
         final PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         final BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
         String inputLine, outputLine, fun = "sin";
+        String[] radians;
         double num = 0;
         while ((inputLine = in.readLine()) != null) {
             System.out.println("Mensaje: " + inputLine);
-            System.out.println(inputLine+" "+ "π \u03C0 "+inputLine.equals("\u03C0"));
-            if (inputLine.startsWith("fun:")){
-                if(inputLine.endsWith("sin"))
-                    fun = "sin";
-                else if (inputLine.endsWith("con"))
-                    fun = "con";
-                else if (inputLine.endsWith("tan"))
-                    fun = "tan";
-                System.out.println("funcion");
-            }
-
+            
             try {
-                Double.parseDouble(inputLine);
+                if (inputLine.contains("pi/")) {
+                    radians = inputLine.split("/");
+                    double numerator = 1;
+                    if (radians[0].indexOf("pi") != 0)
+                        numerator = Double.parseDouble(radians[0].substring(0, radians[0].indexOf("pi")));
+                    num = Math.PI * numerator / Double.parseDouble(radians[1]);
+                } else if (inputLine.contains("fun:")) {
+
+                } else if (inputLine.equals("pi")) {
+                    num = Math.PI;
+                } else
+                    num = Double.parseDouble(inputLine);
+
+                System.out.println("Math.sin(" + num + ")=" + Math.sin(num));
+
+                if (inputLine.startsWith("fun:")) {
+                    if (inputLine.endsWith("sin"))
+                        fun = "sin";
+                    else if (inputLine.endsWith("cos"))
+                        fun = "con";
+                    else if (inputLine.endsWith("tan"))
+                        fun = "tan";
+                    outputLine = "Respuesta: funcion cambiada";
+                } else {
+                    if (fun.equals("sin"))
+                        outputLine = "Respuesta: Sin(" + inputLine + ")= " + String.valueOf(Math.sin(num));
+                    else if (fun.equals("con"))
+                        outputLine = "Respuesta: Cos(" + inputLine + ")= " + String.valueOf(Math.cos(num));
+                    else if (fun.equals("tan"))
+                        outputLine = "Respuesta: Tan(" + inputLine + ")= " + String.valueOf(Math.tan(num));
+                    else
+                        outputLine = "Respuesta: " + inputLine;
+                }
             } catch (NumberFormatException e) {
                 if (inputLine.equals("Bye."))
                     outputLine = "Respuesta: " + inputLine;
                 else
                     outputLine = "Respuesta: " + inputLine + " is not a valid number";
             }
-
-            if (fun.equals("sin"))
-                System.out.println("sin");
-            else if (fun.equals("con"))
-                System.out.println("con");
-            else if (fun.equals("tan"))
-                System.out.println("tan");
-                
-            outputLine = "Respuesta: " + inputLine;
 
             out.println(outputLine);
             if (outputLine.equals("Respuesta: Bye.")) 
--- src/main/java/com/escuelaing/arep/clientServer/HttpServer.java
+++ src/main/java/com/escuelaing/arep/clientServer/HttpServer.java
@@ -4,47 +4,75 @@ import java.net.*;
 import java.io.*;
 
 public class HttpServer {
-  public static void main(String[] args) throws IOException {
-   ServerSocket serverSocket = null;
-   try { 
-      serverSocket = new ServerSocket(36000);
-   } catch (IOException e) {
-      System.err.println("Could not listen on port: 35000.");
-      System.exit(1);
+
+   public static final String USERPATH = System.getProperty("user.dir");
+   public static final String SEPARATOR = System.getProperty("file.separator");
+   public static void main(String[] args) throws IOException {
+      ServerSocket serverSocket = null;
+      try { 
+         serverSocket = new ServerSocket(35000);
+      } catch (IOException e) {
+         System.err.println("Could not listen on port: 35000.");
+         System.exit(1);
+      }
+
+      Socket clientSocket = null;
+      try {
+         System.out.println("Listo para recibir ...");
+         clientSocket = serverSocket.accept();
+      } catch (IOException e) {
+         System.err.println("Accept failed.");
+         System.exit(1);
+      }
+      PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String inputLine, fileName = "/";
+      while ((inputLine = in.readLine()) != null) {
+         System.out.println("Recibí: " + inputLine);
+         if (inputLine.startsWith("GET"))
+            fileName = inputLine.substring(inputLine.indexOf("/")+1, inputLine.indexOf("HTTP"));
+         if (!in.ready()) {
+            break; 
+         }
+      }
+      if (fileName.equals(" "))
+         fileName = "index.html";
+
+      HttpServer.returnFile(fileName, out);
+      out.flush();
+      out.close();
+      in.close();
+      clientSocket.close();
+      serverSocket.close();
+      System.out.println("Close Server");
    }
 
-   Socket clientSocket = null;
-   try {
-       System.out.println("Listo para recibir ...");
-       clientSocket = serverSocket.accept();
-   } catch (IOException e) {
-       System.err.println("Accept failed.");
-       System.exit(1);
-   }
-   PrintWriter out = new PrintWriter(
-                         clientSocket.getOutputStream(), true);
-   BufferedReader in = new BufferedReader(
-                         new InputStreamReader(clientSocket.getInputStream()));
-   String inputLine, outputLine;
-   while ((inputLine = in.readLine()) != null) {
-      System.out.println("Recibí: " + inputLine);
-      if (!in.ready()) {break; }
-   }
-   outputLine = 
-          "<!DOCTYPE html>" + 
+   private static void returnFile(String fileName, PrintWriter out) {
+      String path = HttpServer.USERPATH + HttpServer.SEPARATOR + "src" + HttpServer.SEPARATOR + "main"
+            + HttpServer.SEPARATOR + "java" + HttpServer.SEPARATOR + "resources" + HttpServer.SEPARATOR + fileName;
+
+      out.println("HTTP/1.1 200 OK");
+      out.println("Content-type: " + "text/html");
+      out.println("\r\n");
+      File file = new File(path);
+      try {
+         BufferedReader br = new BufferedReader(new FileReader(file));
+         String st;
+         while ((st = br.readLine()) != null)
+            out.println(st);
+         br.close();
+      } catch (IOException e) {
+         String outputLine = "<!DOCTYPE html>" + 
           "<html>" + 
           "<head>" + 
           "<meta charset=\"UTF-8\">" + 
-          "<title>Title of the document</title>\n" + 
+          "<title>File Not Found</title>\n" + 
           "</head>" + 
           "<body>" + 
-          "<h1>Mi propio mensaje</h1>" + 
+          "<center><h1>File Not Found</h1></center>" + 
           "</body>" + 
-          "</html>" + inputLine; 
-    out.println(outputLine);
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
-  }
+          "</html>";
+          out.println(outputLine);
+      }
+   }
 }
